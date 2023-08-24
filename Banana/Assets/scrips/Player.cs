e861fcd519f26ee79cd1f05bf91e3b8d2298ef62
inhelzer <inhelzer@gmail.com>
2023-08-24 19:11:53 +0300

    input System

--- Banana/Assets/scrips/Player.cs
+++ Banana/Assets/scrips/Player.cs
@@ -1,15 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class Player : MonoBehaviour
+public class Player : MonoBehaviour, Controls.IGameControlsActions
 {
     [SerializeField] Sprite changeSprite;
+    Controls controls;
+
+    private void Awake()
+    {
+        controls = new Controls();
+        controls.GameControls.SetCallbacks(this);
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        controls.GameControls.Enable();
+    }
+
+    private void OnDestroy()
+    {
+        controls.GameControls.Disable();
     }
 
     // Update is called once per frame
@@ -21,5 +34,15 @@ public class Player : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D collision)
     {
         GetComponent<SpriteRenderer>().sprite = changeSprite;
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, 5);
+    }
+
+    public void OnHit(InputAction.CallbackContext context)
+    {
+        throw new System.NotImplementedException();
     }
 }
